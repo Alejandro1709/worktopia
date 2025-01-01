8abd481b17cd92acf6a52e275085c774488da421
Alejandro LR <alejandro.lr.developer@hotmail.com>
2024-12-31 19:21:15 -0500

    Add: Database class

--- public/index.php
+++ public/index.php
@@ -1,14 +1,15 @@
 <?php
   require '../helpers.php';
-
+  require basePath('Database.php');
   require basePath('Router.php');
 
+  // Instatiate the router
   $router = new Router();
-
+  // Get Routes
   $routes = require basePath('routes.php');
-
+  // Get current uri and HTTP Methpd
   $uri = $_SERVER['REQUEST_URI'];
   $method = $_SERVER['REQUEST_METHOD'];
-
+  // Route the request
   $router->route($uri, $method);
 ?>
