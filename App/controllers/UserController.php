6fa62f948b2d3c4a186cba2edaf31d3c7d1b375d
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-01-05 20:01:16 -0500

    Add: Session class

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -4,6 +4,7 @@
 
   use Framework\Database;
   use Framework\Validation;
+  use Framework\Session;
 
   class UserController {
     protected $db;
@@ -107,6 +108,17 @@
 
         $this->db->query('INSERT INTO users (name, email, city, state, password) 
         VALUES (:name, :email, :city, :state, :password)', $params);
+
+        // Get new user ID
+        $userId = $this->db->conn->lastInsertId();
+
+        Session::set('user', [
+          'id' => $userId,
+          'name' => $name,
+          'email' => $email,
+          'city' => $city,
+          'state' => $state
+        ]);
 
         redirect('/');
       }
